a7229016493832c5a2f7e8627848eee0993be952
ViniciusK-PC <viniciuskb36@gmail.com>
2025-01-20 16:01:24 -0300

    Subindo Resoluçoes

---
+++ Exercicio1.sql
@@ -0,0 +1,40 @@
+-- Tabela ALUNO
+CREATE TABLE aluno (
+    matricula NUMERIC(6) PRIMARY KEY,
+    nome VARCHAR(200) NOT NULL,
+    telefone NUMERIC(10),
+    dtnascimento TIMESTAMP,
+    cidade VARCHAR(100)
+);
+
+-- Tabela MATRICULA
+CREATE TABLE matricula (
+    matriculaaluno NUMERIC(6),
+    coddisciplina NUMERIC(6),
+    PRIMARY KEY (matriculaaluno, coddisciplina),
+    FOREIGN KEY (matriculaaluno) REFERENCES aluno(matricula),
+    FOREIGN KEY (coddisciplina) REFERENCES disciplina(cod)
+);
+
+-- Tabela DISCIPLINA
+CREATE TABLE disciplina (
+    cod NUMERIC(6) PRIMARY KEY,
+    nome TEXT NOT NULL,
+    cargahoraria NUMERIC
+);
+
+-- Tabela FUNCIONARIO
+CREATE TABLE funcionario (
+    cod NUMERIC PRIMARY KEY,
+    nome TEXT NOT NULL,
+    cargo TEXT,
+    salario NUMERIC,
+    coddepartamento NUMERIC,
+    FOREIGN KEY (coddepartamento) REFERENCES departamento(cod)
+);
+
+-- Tabela DEPARTAMENTO
+CREATE TABLE departamento (
+    cod NUMERIC PRIMARY KEY,
+    descricao TEXT NOT NULL
+);
