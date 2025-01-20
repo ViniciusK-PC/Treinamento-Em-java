adf842c2c22f6c272584fe8743d1e39af303707a
ViniciusK-PC <viniciuskb36@gmail.com>
2025-01-20 09:22:38 -0300

    Subindo Exercicio 1!

---
+++ Ex1.c
@@ -0,0 +1,7 @@
+// 1) Construa aplicações para realizar o CRUD na tabela Estados
+// CRUD = Create, Retrieve, Update, Delete (Inserir, Retornar, Atualizar, Deletar)
+
+// 2) Pesquise na Internet de códigos que usam C para acessar outros
+// bancos
+
+
